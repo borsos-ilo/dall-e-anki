fdf178273367d186a9071e1274b47fe26027414a
Ilona Kedracka <ilona.kedracka@gmail.com>
2023-11-06 21:34:45 +0100

    basic settings window

--- __init__.py
+++ __init__.py
@@ -5,19 +5,36 @@ from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+def show_config_dialog():
+    config = mw.addonManager.getConfig(__name__)
+    dialog = QDialog(mw)
 
-def testFunction() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
+    layout = QVBoxLayout(dialog)
+    label = QLabel("This is a settings window for your add-on!", dialog)
+    layout.addWidget(label)
 
-# create a new menu item, "test"
-action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
-# and add it to the tools menu
-mw.form.menuTools.addAction(action)
+    dialog.setLayout(layout)
+    dialog.exec_()
+
+# This registers the config dialog function as the one to be called
+# when the user clicks the "Config" button in Anki's add-on list.
+mw.addonManager.setConfigAction(__name__, show_config_dialog)
+
+
+
+# # We're going to add a menu item below. First we want to create a function to
+# # be called when the menu item is activated.
+
+# def testFunction() -> None:
+#     # get the number of cards in the current collection, which is stored in
+#     # the main window
+#     cardCount = mw.col.cardCount()
+#     # show a message box
+#     showInfo("Card count: %d" % cardCount)
+
+# # create a new menu item, "test"
+# action = QAction("test", mw)
+# # set it to call testFunction when it's clicked
+# qconnect(action.triggered, testFunction)
+# # and add it to the tools menu
+# mw.form.menuTools.addAction(action)
